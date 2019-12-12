7768c7fa795b1dd00ca19aa7d2cabd3a592ac8ee
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2019-12-12 23:48:26 +0100

    Debug

---
+++ recipes-api/infra/lib/s3.js
@@ -0,0 +1,38 @@
+const R = require('ramda');
+const fs = require('fs');
+const { s3 } = require('./aws');
+
+const S3_BUCKET = 'microservices-school-recipes-carlos';
+
+const registerInstance = (publicDns) => {
+  const registry = {
+    creationDate: new Date(),
+    publicDns
+  };
+  const params = {
+    Body: JSON.stringify(registry), 
+    Bucket: S3_BUCKET, 
+    Key: 'instances-registry'
+  };
+  return new Promise((resolve, reject) => {
+    s3.putObject(params, (err) => (err ? reject(err) : resolve()));
+  });
+};
+
+const download = R.curry((s3Key, outputFile) => new Promise((resolve, reject) => {
+  const s3Stream = s3.getObject({ Bucket: S3_BUCKET, Key: s3Key }).createReadStream();
+  const fileStream = fs.createWriteStream(outputFile);
+  s3Stream.on('error', reject);
+  fileStream.on('error', reject);
+  fileStream.on('close', () => resolve(outputFile));
+  s3Stream.pipe(fileStream);
+}));
+
+const downloadPemFile = download('keys/microservices1.pem');
+const downloadInstanceRegistry = download('instances-registry');
+
+module.exports = {
+  downloadPemFile,
+  downloadInstanceRegistry,
+  registerInstance
+};
